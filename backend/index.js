ee5bb8dc77ad2e8a3354fd324d679be81bd701b6
NikBon-FE <n.bontemps33600@gmail.com>
2024-11-27 09:42:11 +0100

    27/11/24 - 03 - login.html, base d'index.js

--- backend/index.js
+++ backend/index.js
@@ -15,3 +15,38 @@ __copyright__ = "Unilasalle Amiens"
 __date__ = "2024-??-??"
 __version__= "1.0.0"
 
+const express = require('express');
+const mariadb = require('mariadb');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+// Utiliser le middleware CORS
+app.use(cors());
+
+// Utiliser le middleware pour traiter les requêtes JSON
+app.use(express.json());
+
+// Configuration de la connexion à MariaDB
+const pool = mariadb.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: '',   // <-- INSERER LE NOM DE LA DATABASE ICI
+    connectionLimit: 5
+});
+
+// Tester la connexion à la base de données
+pool.getConnection()
+    .then(conn => {
+        console.log("Connecté à MariaDB !");
+        conn.release();
+    })
+    .catch(err => {
+        console.error("Erreur de connexion à MariaDB", err);
+    });
+
+app.listen(port, () => {
+    console.log(`Serveur démarré sur http://localhost:${port}`);
+});
