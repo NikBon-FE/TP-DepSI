976c3416bb315dc8295e54b6e831623901f0602c
NikBon-FE <n.bontemps33600@gmail.com>
2024-11-27 08:44:35 +0100

    27/11/24 - 02 - Debut Backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,17 @@
-test
+/**Back-end ; utilisé pour traiter les différentes commandes database
+
+Usage:
+======
+    Javascript index.js
+
+    (inserer ici chaque fonction de push/pull)
+    
+    
+**/
+
+__authors__ = ("Nicolas Bontemps, Alexis Klucj")
+__contact__ = ("nicolas.bontemps@etu.unilasalle.fr, alexis.kluck@etu.unilasalle.fr")
+__copyright__ = "Unilasalle Amiens"
+__date__ = "2024-??-??"
+__version__= "1.0.0"
+
